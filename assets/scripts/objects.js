b8d470652fd80b9d4a42a2d1c75377d37b41299c
Leonardo Chonkan <lchonkan@forgebim.com>
2020-08-18 00:24:16 -0600

    implements add movie event handler and functionlity

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,13 +1,38 @@
-let person = {
-    name: 'Max',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    greet: function () {
-        alert('Hi There');
-    },
+// Select Elements from the DOM
+const addMovieBtn = document.getElementById('add-movie-btn');
+const serachBtn = document.getElementById('search-btn');
+
+//Defining global variables
+const movies = [];
+
+//Function definitions
+
+const addMovieHandler = () => {
+    //We want to grab the user input and the add them to the movie
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    //We perform some validation on the inputs before attempting to use them
+    if (
+        title.trim() === '' ||
+        extraName.trim() === '' ||
+        extraValue.trim() === ''
+    ) {
+        return;
+    }
+
+    //construct a new movie
+
+    const newMovie = {
+        info: { title, [extraName]: extraValue },
+        id: Math.random(),
+    };
+
+    movies.push(newMovie);
+    console.log(newMovie);
 };
 
-person.isAdmin = true;
-delete person.age;
+//Add event listeners to buttons
+addMovieBtn.addEventListener('click', addMovieHandler);
 
-console.log(person);
